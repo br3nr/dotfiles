8684bab1062d7d5808588e6f5a48170c84901fde
br3nr <repetitive_routine@protonmail.com>
2023-09-09 21:05:44 +0800

    Configs to fix hyperland nvidia issues

---
+++ .config/systemd/user/set-env-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export _java_awt_wm_noreparenting=1                                             
+export XDG_SESSION_TYPE=wayland                                                 
+export XDG_CURRENT_SESSION_TYPE=wayland                                         
+export XDG_CURRENT_DESKTOP=Hyprland                                             
+export XDG_SESSION_DESKTOP=Hyprland                                             
+export __GLX_VENDOR_LIBRARY_NAME=nvidia                                         
+export GMB_BACKEND=nvidia-drm                                                   
+export WLR_RENDERER_ALLOW_SOFTWARE=1    
