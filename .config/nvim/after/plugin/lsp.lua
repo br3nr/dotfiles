20231d01aa3c0f3acbeee63743350d88e2fd03fd
br3nr <repetitive_routine@protonmail.com>
2023-09-17 21:16:31 +0800

    Add rosepine theme w new keybinds for vim

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -29,7 +29,6 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 local cmp_action = require('lsp-zero').cmp_action()
