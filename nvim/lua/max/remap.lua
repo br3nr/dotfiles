eece055426d6be35d50a80176a8e8cfb311dd744
br3nr <repetitive_routine@protonmail.com>
2025-10-22 21:05:22 +0800

    move out of config

---
+++ nvim/lua/max/remap.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Copy to clipboard
+vim.api.nvim_set_keymap('x', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg_', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>yy', '"+yy', { noremap = true, silent = true })
+
+-- Paste from clipboard
+vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', '<leader>P', '"+P', { noremap = true, silent = true })
