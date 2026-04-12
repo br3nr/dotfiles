ce3a74dc27ea249e85a69ff3f02202ded7268aea
br3nr <repetitive_routine@protonmail.com>
2026-04-12 12:49:01 +0800

    remove depracated packer && add make config more sick

---
+++ nvim/lua/max/lazy/colors.lua
@@ -0,0 +1,19 @@
+function ColorNeovim(color)
+  color = color or "cyberdream"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    name = "cyberdream",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      ColorNeovim()
+    end,
+  },
+}
